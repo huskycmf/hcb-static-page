24000b7ed7df1a2a848eea4da1169f84ec08a8c2
Oleksii Mylotskyi <spalaxinco@gmail.com>
2014-03-23 13:39:39 +0200

    Common it is not a good name for part of router, name must describe purpose.

--- config/module/router.config.php
+++ config/module/router.config.php
@@ -3,7 +3,7 @@ return array(
     'routes' => array(
         'hc-backend' => array(
             'child_routes' => array(
-                'page' => include __DIR__ . '/router/common.config.php'
+                'page' => include __DIR__ . '/router/static-page.config.php'
             )
         )
     )
